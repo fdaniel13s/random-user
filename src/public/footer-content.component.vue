22c92befc4d6a3f3a463611e84abec1ef40007a9
fdaniel13s <u20221c628@upc.edu.pe>
2024-04-19 14:48:10 -0500

    footer designed

--- src/public/footer-content.component.vue
+++ src/public/footer-content.component.vue
@@ -5,11 +5,16 @@ export default {
 </script>
 
 <template>
-  <div class="bg-cyan-300">
-    <div class="text-color">
-      <h1>Developed by U20221C628-UPC</h1>
+  <div class="grid bg-primary mt-1 p-1 align-content-start border-round">
+    <div class="col-12 ml-3 mt-2 align-items-center justify-content-center">
+      <p>Copyright &copy 2024. Rest Countries </p>
     </div>
+    <div class="col-12 ml-3 align-items-center justify-content-center">
+      <p>
+        Developed by Fernando Daniel Quispe Condori U20221C628
+      </p>
     </div>
+  </div>
 
 </template>
 
